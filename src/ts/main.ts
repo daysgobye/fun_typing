3b77fdeb5f359838d8ea3e76ec39a69195c82968
cole smith <38364556+daysgobye@users.noreply.github.com>
2023-02-20 21:22:43 -0800

    Initial commit

---
+++ src/ts/main.ts
@@ -0,0 +1,31 @@
+// Import Base file
+import { registerSW } from 'virtual:pwa-register';
+import 'virtual:svg-icons-register';
+
+// Import SCSS
+import '/scss/site.scss';
+
+// Import Libraries
+import './libs/smoothScroll';
+import './libs/modal';
+
+// Import Namespace
+import sample from './namespace/sample';
+
+// Call Namespace
+const checkSamplePage = document.getElementById('sample');
+if (checkSamplePage) {
+  sample();
+}
+
+// Register SW
+const updateSW = registerSW({
+  onNeedRefresh() {
+    console.log('onNeedRefresh');
+  },
+  onOfflineReady() {
+    console.log('onOfflineReady');
+  },
+});
+
+void updateSW();
